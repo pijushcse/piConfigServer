aaaad1a2b840eacdb0ae373b82a8554560aeab33
pijushcse <pijushcse@CND50814ZJ.lca.labcorp.com>
2016-12-08 17:28:11 -0500

    enable basic http authentication

--- src/main/java/com/pilab/xyz/cloud/config/Main.java
+++ src/main/java/com/pilab/xyz/cloud/config/Main.java
@@ -1,4 +1,4 @@
-package com.pilab.xyz;
+package com.pilab.xyz.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
