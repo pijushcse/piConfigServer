d5e58406ab2778e28cf7afa1f96d3270541df12f
pijushcse <pijushcsecuet@gmail.com>
2016-12-08 08:06:28 -0500

    hello

---
+++ src/main/java/com/pilab/xyz/Main.java
@@ -0,0 +1,14 @@
+package com.pilab.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
